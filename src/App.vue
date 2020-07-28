5e24e34ac268ebed656740200140dba88612b29e
Chris Lewis <chrissam42@gmail.com>
2020-07-28 12:47:01 -0700

    rename HelloWorld to GrandTour

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <GrandTour msg="Type Network Grand Tour" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import GrandTour from "./components/GrandTour.vue";
 
 export default {
-  name: "App",
+  name: "Grand Tour",
   components: {
-    HelloWorld
+    GrandTour
   }
 };
 </script>
--- src/components/GrandTour.vue
+++ src/components/GrandTour.vue
@@ -88,7 +88,7 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "GrandTour",
   props: {
     msg: String
   }
