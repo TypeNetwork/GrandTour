85910cd80507ffa3fa32171a69f7db747662fe73
Chris Lewis <chrissam42@gmail.com>
2020-07-29 16:24:59 -0700

    improve knuth linebreaking demo

---
+++ src/components/demos/LineBreaking.js
@@ -0,0 +1,74 @@
+import { layoutItemsFromString, breakLines /*, positionItems */ } from 'tex-linebreak';
+
+export default {
+  name: "LineBreaking",
+  data: () => ({
+    ruler: null,
+    knuthLines: [],
+    paragraphWidth: 20,
+    justify: false,
+  }),
+  computed: {
+  },
+  methods: {
+    doKnuth() {
+      //"this" will be the vue object
+      const lineWidth = this.$refs.originalParagraph.clientWidth;
+      const items = layoutItemsFromString(this.$refs.originalParagraph.textContent, this.measureText);
+      const breakpoints = breakLines(items, lineWidth);
+    
+      let lines = [];
+      for (let i=0, l=breakpoints.length-1; i < l; i++) {
+        lines.push(items.slice(breakpoints[i], breakpoints[i+1]).map(item => item.text ?? "").join("").trim());
+      }
+      
+      var knuthLines = [];
+      
+      for (let text of lines) {
+        let line = {
+          text,
+          style: {},
+        };
+
+        const actualWidth = this.measureText(text);
+        const words = text.trim().split(/[\s-]+/);
+        if (words.length > 1) {
+          const wordSpace = (lineWidth - actualWidth) / (words.length - 1);
+          if (this.justify || wordSpace < 0) {
+            line.style.wordSpacing = wordSpace + 'px';
+          }
+        }
+        knuthLines.push(line);
+      }
+      
+      if (knuthLines.length > 0) {
+        knuthLines[knuthLines.length-1].style.wordSpacing = "";
+      }
+      
+      this.knuthLines = knuthLines;
+      this.xtraLines = knuthLines;
+    },
+    measureText(text) {
+      if (text === " ") {
+        return this.measureText("H H") - this.measureText("HH");
+      }
+      this.ruler.textContent = text;
+      const result = this.ruler.getBoundingClientRect().width;
+      this.ruler.textContent = "";
+      return result;
+    },
+  },
+  mounted: function() {
+    this.ruler = document.createElement('span');
+    this.ruler.style.position = "absolute";
+    this.ruler.style.visibility = "hidden";
+    this.ruler.style.whiteSpace = "nowrap";
+    this.$refs.originalParagraph.appendChild(this.ruler);
+    
+    this.doKnuth();
+    window.addEventListener('resize', this.doKnuth);
+  },
+  beforeDestroy: function() {
+    window.removeEventListener('resize', this.doKnuth);
+  }
+};
--- src/components/demos/LineBreaking.vue
+++ src/components/demos/LineBreaking.vue
@@ -1,105 +1,4 @@
-<template>
-  <div class='line-breaking-demo'>
-    <label>
-      Paragraph width:
-      <input type="range" v-model="paragraphWidth" min="10" max="60" value="20" @change="doKnuth"> em
-    </label>
-    <label>
-      Justify? <input type="checkbox" v-model="justify">
-    </label>
-    <h4>Original:</h4>
-    <p class='original' :class="{ justified: justify }" :style="{maxWidth: paragraphWidth + 'em'}">
-      <slot>
-        There are many variations of passages of Lorem Ipsum available, 
-        but the majority have suffered alteration in some form, by injected humour, 
-        or randomised words which don't look even slightly believable. 
-        If you are going to use a passage of Lorem Ipsum, you need to be sure there 
-        isn't anything embarrassing hidden in the middle of text. 
-        All the Lorem Ipsum generators on the Internet tend to repeat predefined 
-        chunks as necessary, making this the first true generator on the Internet. 
-        It uses a dictionary of over 200 Latin words, combined with a handful of 
-        model sentence structures, to generate Lorem Ipsum which looks reasonable. 
-        The generated Lorem Ipsum is therefore always free from repetition, 
-        injected humour, or non-characteristic words etc.
-      </slot>
-    </p>
-    
-    <h4>Knuth’d:</h4>
-    <p :class="{ output: true, justified: justify }" :style="{maxWidth: paragraphWidth + 'em'}"></p>
-  </div>
-</template>
-
-<script>
-
-import { layoutItemsFromString, breakLines /*, positionItems */ } from 'tex-linebreak';
-
-export default {
-  name: "LineBreaking",
-  data: () => ({
-    ruler: null,
-    outputHTML: "",
-    paragraphWidth: 20,
-    justify: false,
-  }),
-  computed: {
-    originalParagraph: function() {
-      return this.$el.querySelector('p.original');
-    },
-    outputParagraph: function() {
-      return this.$el.querySelector('p.output');
-    },
-  },
-  methods: {
-    doKnuth() {
-      //"this" will be the vue object
-      const items = layoutItemsFromString(this.originalParagraph.textContent, this.measureText);
-      const breakpoints = breakLines(items, this.originalParagraph.getBoundingClientRect().width);
-    
-      let lines = [];
-      for (let i=0, l=breakpoints.length-1; i < l; i++) {
-        lines.push(items.slice(breakpoints[i], breakpoints[i+1]).map(item => item.text ?? "").join("").trim());
-      }
-      
-      this.outputParagraph.innerHTML = "<div>" + lines.join("</div><div>") + "</div>";
-      
-      const fullWidth = this.outputParagraph.clientWidth;
-      for (let line of this.outputParagraph.childNodes) {
-        const actualWidth = this.measureText(line.textContent);
-        const words = line.textContent.trim().split(/[\s-]+/);
-        if (words.length > 1) {
-          const wordSpace = (fullWidth - actualWidth) / (words.length - 1);
-          line.style.wordSpacing = wordSpace + 'px';
-        }
-      }
-    },
-    measureText(text) {
-      if (text === " ") {
-        return this.measureText("H H") - this.measureText("HH");
-      }
-      this.ruler.textContent = text;
-      const result = this.ruler.getBoundingClientRect().width;
-      this.ruler.textContent = "";
-      return result;
-    },
-  },
-  mounted: function() {
-    this.ruler = document.createElement('span');
-    this.ruler.style.position = "absolute";
-    this.ruler.style.visibility = "hidden";
-    this.ruler.style.whiteSpace = "nowrap";
-    this.originalParagraph.appendChild(this.ruler);
-    
-    this.doKnuth();
-    window.addEventListener('resize', this.doKnuth);
-  },
-  beforeDestroy: function() {
-    window.removeEventListener('resize', this.doKnuth);
-  }
-};
-
-</script>
-
-<style lang="scss">
+<style lang="scss" scoped>
 .line-breaking-demo {
   label {
     display: block;
@@ -118,17 +17,56 @@ export default {
     text-align: justify;
   }
   
-  p.output {
-    white-space: nowrap;
-  }
-  
-  p.output:not(.justified) > * {
-    word-spacing: 0 !important;
-  }
-  
-  p.output.justified > :last-child {
-    word-spacing: 0 !important;
+  p.knuth, p.xtra {
+    > * {
+      white-space: nowrap;
+      display: block;
+      white-space: nowrap;
+      width: max-content;
+    }
   }
 }
-
 </style>
+
+<template>
+  <div class='line-breaking-demo'>
+    <label>
+      Paragraph width:
+      <input type="range" v-model="paragraphWidth" min="10" max="60" value="20" step="0.1" @input="doKnuth"> em
+    </label>
+    <label>
+      Justify? <input type="checkbox" v-model="justify" @change="doKnuth">
+    </label>
+    <h4>Original:</h4>
+    <p ref="originalParagraph" class='original' :class="{ justified: justify }" :style="{maxWidth: paragraphWidth + 'em'}">
+      <slot>
+        There are many variations of passages of Lorem Ipsum available, 
+        but the majority have suffered alteration in some form, by injected humour, 
+        or randomised words which don't look even slightly believable. 
+        If you are going to use a passage of Lorem Ipsum, you need to be sure there 
+        isn't anything embarrassing hidden in the middle of text. 
+        All the Lorem Ipsum generators on the Internet tend to repeat predefined 
+        chunks as necessary, making this the first true generator on the Internet. 
+        It uses a dictionary of over 200 Latin words, combined with a handful of 
+        model sentence structures, to generate Lorem Ipsum which looks reasonable. 
+        The generated Lorem Ipsum is therefore always free from repetition, 
+        injected humour, or non-characteristic words etc.
+      </slot>
+    </p>
+    
+    <h4>Knuth + wordspace:</h4>
+    <p :class="[ 'knuth', justify ? 'justified' : 'ragged' ]" :style="{maxWidth: paragraphWidth + 'em'}">
+      <span v-for="(line, i) in knuthLines" :key="i" :style="line.style">{{line.text}}</span>
+    </p>
+
+<!--
+    <h4>Knuth + XTRA:</h4>
+    <p class="xtra" :style="{maxWidth: paragraphWidth + 'em'}">
+      <span v-for="(line, i) in xtraLines" :key="i" :style="line.style">{{line.text}}</span>
+    </p>
+-->
+  </div>
+</template>
+
+<script src="./LineBreaking.js"/>
+
